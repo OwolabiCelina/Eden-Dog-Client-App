5c0985064bf8595ca7f2c74a4df079d22c918161
Owolabi Celina <108185066+OwolabiCelina@users.noreply.github.com>
2023-07-16 18:53:00 +0100

    DogCard and HomeView added

--- src/components/DogCard.vue
+++ src/components/DogCard.vue
@@ -1,3 +1,39 @@
 <template>
-    <h1>Dog here</h1>
+    <div class="card-container">
+        <div class="card-image" :style="`background-image: url(${imageUrl})`">
+            <slot></slot>
+        </div>
+        <p>Save Icon</p>
+    </div>
 </template>
+
+<script>
+export default {
+  props: {
+    imageUrl: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.card-container {
+  background-color: rgb(202, 201, 201);
+  width: 300px;
+  height: 450px;
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+}
+.card-image {
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  flex: 1;
+}
+p {
+  margin: 10px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,55 @@
 <template>
     <div class="home">
-      <h1>Dogs</h1>
-      <DogCard />
+      <h1>EdenSpa</h1>
+      <div class="card-row">
+        <DogCard v-for="dog of dogs" :key="dog" :image-url="dog">
+          <!-- Content for the card -->
+        </DogCard>
+      </div>
     </div>
   </template>
   
   <script>
   import DogCard from '@/components/DogCard.vue';
+  import axios from 'axios'
   
   export default {
     name: 'HomeView',
     components: {
-      DogCard
+      DogCard,
+    },
+    data(){
+      return {
+        dogs: [],
+        folder: ""
+      }
+    },
+    created() {
+      this.loadImages()
+    },
+    methods: {
+      async loadImages(){
+        for(let i = 0; i < 150; i++){
+          await axios.get("https://dog.ceo/api/breeds/image/random")
+            .then((response) => {
+              this.dogs.push(response.data.message)
+            })
+        }
+      }
+  
     }
   }
   </script>
+  <style scoped>
+  .home {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
   
+  .card-row {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  </style>
