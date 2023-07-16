6a3e6e4fa8d808b54a1a7d2f3b33f229dd80c091
Owolabi Celina <108185066+OwolabiCelina@users.noreply.github.com>
2023-07-16 05:27:44 +0100

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="home">
+      <h1>Dogs</h1>
+      <DogCard />
+    </div>
+  </template>
+  
+  <script>
+  import DogCard from '@/components/DogCard.vue';
+  
+  export default {
+    name: 'HomeView',
+    components: {
+      DogCard
+    }
+  }
+  </script>
+  
